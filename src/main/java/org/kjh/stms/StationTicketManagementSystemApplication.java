8ad667edf6ec44f8be664b4746f5be8491313f52
kejinhong <3340201045@qq.com>
2024-12-02 19:49:55 +0800

    Initial commit

---
+++ src/main/java/org/kjh/stms/StationTicketManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.kjh.stms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationTicketManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StationTicketManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kjh/stms/StationTicketManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kjh.stms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationTicketManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
